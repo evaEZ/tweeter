769b94de4d94eede24c395ffacfd9a4049b4110f
evaEZ <eva.jingzhao@gmail.com>
2020-10-09 04:02:00 +0000

    update the counter color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,12 @@ $(document).ready(function() {
   // --- our code goes here ---
   let str = 0;
   $('textarea').keyup(function(){
-  //console.log($(this).val());
-  str = $(this).length;
-  //console.log( $('.counter').text());
-  //console.log($(this).parent().find('.counter').text());
-  let count = $(this).parent().find('.counter').text();
-  const remain = count - str;
-  $(this).parent().find('.counter').text(remain);
-   
-  //console.log(str);
+    str = $(this).length;
+    let count = $(this).parent().find('.counter').text();
+    const remain = count - str;
+    if (remain < 0) {
+      $('output').css("color", "#D8000C");
+    };
+    $(this).parent().find('.counter').text(remain);
   });
-  //const valueTextarea = $('.section form textarea');
-  //console.log(valueTextarea);
-  //valueTextarea.focus(focusHandler);
 });
